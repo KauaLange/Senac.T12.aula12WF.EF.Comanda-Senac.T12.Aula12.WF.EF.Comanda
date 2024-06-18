ac059d2a8f9247ef1be44057a0547a79e68bb912
KauaLange <160553514+KauaLange@users.noreply.github.com>
2024-06-18 19:21:47 -0300

    Add files via upload

---
+++ FrmCardapio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemaDeComandas
+{
+    public partial class FrmCardapio : Form
+    {
+        public FrmCardapio()
+        {
+            InitializeComponent();
+        }
+
+        private void cyberGroupBox1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void spaceForm1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnVoltar_Click(object sender, EventArgs e)
+        {
+            // fecha essa tela
+            Close();
+        }
+    }
+}
